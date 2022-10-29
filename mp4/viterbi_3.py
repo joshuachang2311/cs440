bf1062c10e69b765e107d808ad504b16229523e9
Joshua Chang <chchang6@illinois.edu>
2022-10-28 23:52:09 -0500

    Pause on viterbi_3

--- mp4/viterbi_3.py
+++ mp4/viterbi_3.py
@@ -7,13 +7,22 @@ with enhancements such as dealing with suffixes/prefixes separately
 """
 
 
-def viterbi_3(train, test, a=1e-5):
+def get_token_name(word, suffixes):
+    for suffix in suffixes:
+        if word[-len(suffix):] == suffix:
+            return f"hapax-{suffix}"
+    return word
+
+
+def viterbi_3(train, test, a=1):
     """
     input:  training data (list of sentences, with tags on the words). E.g.,  [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
             test data (list of sentences, no tags on the words). E.g.,  [[word1, word2], [word3, word4]]
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     """
+    suffixes = ["ed", "ly", "ic", "ing", "ous", "tion", "ness", "able", ",000", "ment", "tions", "ments", "ility", "-like", "-old"]
+
     tags = set()
     for sentence in train:
         tags = tags.union(set([tag for _, tag in sentence]))
@@ -37,11 +46,26 @@ def viterbi_3(train, test, a=1e-5):
                 word_counts[word] = 0
             word_counts[word] += 1
     hapax_words = set([word for word in word_counts if word_counts[word] == 1])
+    real_hapax_words = set([word for word in hapax_words if get_token_name(word, suffixes) == word])
+    # print("hapax words:")
+    # print()
+    # for suffix_length in range(2, 6):
+    #     print("suffix length =", suffix_length)
+    #     suffix_count = {}
+    #     for word in hapax_words:
+    #         suffix = word[-suffix_length:]
+    #         if suffix not in suffix_count:
+    #             suffix_count[suffix] = 0
+    #         suffix_count[suffix] += 1
+    #     for suffix, count in sorted(list(suffix_count.items()), key=lambda x: -x[1])[:20]:
+    #         print(suffix, count)
+    #     print()
+
     # print(len(hapax_words))
     ps_hapax = np.array([0 for _ in range(len(tags))], dtype=float)
     for sentence in train:
         for word, tag in sentence:
-            if word in hapax_words:
+            if word in real_hapax_words:
                 ps_hapax[tag_to_index[tag]] += 1
     v = np.sum(ps_hapax > 0)
     n = np.sum(ps_hapax)
@@ -64,12 +88,24 @@ def viterbi_3(train, test, a=1e-5):
                     pe[tag_1] = {}
                 if word_1 not in pe[tag_1]:
                     pe[tag_1][word_1] = 0
+                if word_1 in hapax_words:
+                    token_1 = get_token_name(word_1, suffixes)
+                    if token_1 is not None:
+                        if token_1 not in pe[tag_1]:
+                            pe[tag_1][token_1] = 0
+                        pe[tag_1][token_1] += 1
                 pe[tag_1][word_1] += 1
 
             if tag_2 not in pe:
                 pe[tag_2] = {}
             if word_2 not in pe[tag_2]:
                 pe[tag_2][word_2] = 0
+            if word_2 in hapax_words:
+                token_2 = get_token_name(word_2, suffixes)
+                if token_2 is not None:
+                    if token_2 not in pe[tag_2]:
+                        pe[tag_2][token_2] = 0
+                    pe[tag_2][token_2] += 1
             pe[tag_2][word_2] += 1
 
     for tag_1 in pt:
@@ -82,6 +118,8 @@ def viterbi_3(train, test, a=1e-5):
     pt[None] = {tag: 1 / len(tags) for tag in tags}
 
     for tag in pe:
+        # print(tag)
+        # print([tup for tup in pe[tag].items() if tup[0][:5] == "hapax"])
         v = len(pe[tag])
         n = sum(pe[tag].values())
         alpha = a * ps_hapax[tag_to_index[tag]]
@@ -93,14 +131,21 @@ def viterbi_3(train, test, a=1e-5):
     for sentence in test:
         v, b = np.full((len(sentence), len(tags)), 0, dtype=float), np.full((len(sentence), len(tags)), 0, dtype=int)
         for i, tag in enumerate(tags):
-            v[0, i] = math.log(ps[i]) + math.log(pe[tag][sentence[0] if sentence[0] in pe[tag] else None])
+            v[0, i] = math.log(ps[i]) + \
+                math.log(pe[tag][
+                    sentence[0] if sentence[0] in pe[tag] else
+                    get_token_name(sentence[0], suffixes) if get_token_name(sentence[0], suffixes) in pe[tag] else None
+                ])
         for i, word in enumerate(sentence[1:], start=1):
             for i_tag_2, tag_2 in enumerate(tags):
                 vs = np.array([
                     v[i - 1, i_tag_1] +
                     math.log(pt[tag_1 if tag_1 in pt else None]
-                           [tag_2 if tag_2 in pt[tag_1 if tag_1 in pt else None] else None]) +
-                    math.log(pe[tag_2][word if word in pe[tag_2] else None])
+                             [tag_2 if tag_2 in pt[tag_1 if tag_1 in pt else None] else None]) +
+                    math.log(pe[tag_2][
+                        word if word in pe[tag_2] else
+                        get_token_name(word, suffixes) if get_token_name(word, suffixes) in pe[tag_2] else None
+                    ])
                     for i_tag_1, tag_1 in enumerate(tags)
                 ])
                 b[i, i_tag_2] = np.argmax(vs)
