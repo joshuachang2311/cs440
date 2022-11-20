8bf4d48669a45c830d1845edc9b40531c7f71698
Joshua Chang <chchang6@illinois.edu>
2022-11-20 13:32:28 -0600

    Initialize mp6

---
+++ mp6/agent.py
@@ -0,0 +1,61 @@
+import numpy as np
+import utils
+
+class Agent:    
+    def __init__(self, actions, Ne=40, C=40, gamma=0.7):
+        # HINT: You should be utilizing all of these
+        self.actions = actions
+        self.Ne = Ne # used in exploration function
+        self.C = C
+        self.gamma = gamma
+        self.reset()
+        # Create the Q Table to work with
+        self.Q = utils.create_q_table()
+        self.N = utils.create_q_table()
+        
+    def train(self):
+        self._train = True
+        
+    def eval(self):
+        self._train = False
+
+    # At the end of training save the trained model
+    def save_model(self,model_path):
+        utils.save(model_path,self.Q)
+        utils.save(model_path.replace('.npy', '_N.npy'), self.N)
+
+    # Load the trained model for evaluation
+    def load_model(self,model_path):
+        self.Q = utils.load(model_path)
+
+    def reset(self):
+        # HINT: These variables should be used for bookkeeping to store information across time-steps
+        # For example, how do we know when a food pellet has been eaten if all we get from the environment
+        # is the current number of points? In addition, Q-updates requires knowledge of the previously taken
+        # state and action, in addition to the current state from the environment. Use these variables
+        # to store this kind of information.
+        self.points = 0
+        self.s = None
+        self.a = None
+    
+    def act(self, environment, points, dead):
+        '''
+        :param environment: a list of [snake_head_x, snake_head_y, snake_body, food_x, food_y] to be converted to a state.
+        All of these are just numbers, except for snake_body, which is a list of (x,y) positions 
+        :param points: float, the current points from environment
+        :param dead: boolean, if the snake is dead
+        :return: chosen action between utils.UP, utils.DOWN, utils.LEFT, utils.RIGHT
+
+        Tip: you need to discretize the environment to the state space defined on the webpage first
+        (Note that [adjoining_wall_x=0, adjoining_wall_y=0] is also the case when snake runs out of the playable board)
+        '''
+        s_prime = self.generate_state(environment)
+
+        # TODO: write your function here
+
+        return utils.RIGHT
+
+    def generate_state(self, environment):
+        # TODO: Implement this helper function that generates a state given an environment 
+
+        return None
